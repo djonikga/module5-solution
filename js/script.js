0f3b104cc15e18a5501cc5302061c356841e224c
djonikga <djonikga@gmail.com>
2020-01-04 14:47:24 +0300

    fix randomCategoryShortName
    Signed-off-by: djonikga <djonikga@gmail.com>

--- js/script.js
+++ js/script.js
@@ -102,7 +102,6 @@ function buildAndShowHomeHTML (categories) {
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
-      //var randomCategoryShortName = chosenCategoryShortName.short_name;
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -117,7 +116,6 @@ function buildAndShowHomeHTML (categories) {
       //
       chosenCategoryShortName = "'" + chosenCategoryShortName + "'";
       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
-      //var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "short_name", randomCategoryShortName);
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
